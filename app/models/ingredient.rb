15a76fc11fe71f09e1f7d6da8cd5e771845a4f70
Pierre-Christophe Callac <pccindo78@msn.com>
2017-02-16 15:46:47 +0100

    add model cocktail dose ingredient

---
+++ app/models/ingredient.rb
@@ -0,0 +1,6 @@
+class Ingredient < ApplicationRecord
+    has_many :doses, dependent: :destroy
+    has_many :cocktails, through: :doses
+
+  validates :name, presence: true, uniqueness: true
+end
---
+++ db/migrate/20170216141445_create_doses.rb
@@ -0,0 +1,10 @@
+class CreateDoses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :doses do |t|
+      t.text :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
